952b9e3434eb9e61f914b8c03ed8015c00b3f21e
Kazu Takahashi <num96kazu19@icloud.com>
2022-01-16 15:36:42 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const slider = document.querySelector(".container");
+let isDown = false;
+let startx;
+let scrollToLeft;
+
+slider.addEventListener("mousedown", function (e) {
+  isDown = true;
+  slider.classList.add("active");
+
+  //   console.log(e);
+  startx = e.pageX - slider.offsetLeft;
+
+  scrollToLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseup", function () {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseleave", function () {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", function (e) {
+  if (!isDown) return;
+  e.preventDefault();
+
+  const distanceX = e.pageX - slider.offsetLeft;
+  const walk = distanceX - startx;
+  //   console.log(walk);
+  slider.scrollLeft = scrollToLeft - walk;
+});
